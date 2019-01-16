5ac64f8104fdb9ea0a3613e85cf0c2182ef17dc4
Santi <tau150@hotmail.com>
2019-01-16 14:49:01 -0300

    axios instance

--- src/db/utils.js
+++ src/db/utils.js
@@ -1,6 +1,12 @@
+import axios from 'axios';
+
 export const requireAll = ctx =>
-  ctx
-    .keys()
-    .map(ctx)
-    .map(m => m.default)
-    .filter(Boolean);
+	ctx
+		.keys()
+		.map(ctx)
+		.map(m => m.default)
+		.filter(Boolean);
+
+export const axiosInstance = axios.create({
+	baseURL: 'https://football-players-b31f2.firebaseio.com/players.json?print=pretty'
+});
