f951b413d00d89c45c6216119fc7dafb5cc72ef0
Santi Nuñez <tau150@hotmail.com>
2019-01-20 11:08:51 -0300

    enzyme config

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders 1 <App /> component', () => {
+    const component = shallow(<App />);
+    expect(component).toHaveLength(1);
+  });
 });
